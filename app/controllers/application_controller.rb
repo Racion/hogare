dca4ba03c5285044d0e4c519fd79037940e9ffd9
Rafael H. Osorio <rafa.osorioa@gmail.com>
2021-03-02 15:21:07 -0500

    Devise configured

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permited_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permited_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :dni, :phone, :address])
+  end
 end
